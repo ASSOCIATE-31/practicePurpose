b90c087b339dbda2752831ebb79f92b7441c4458
Arka Bhattacharyya <arka@digitalaptech.co.in>
2023-11-24 14:10:07 +0530

    Resolved from developer_two 3

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,12 +1,10 @@
 From developer_two  
 From developer_one  
 From dev branch
-
 Make conflict loper_two
 Make conflict developer_one
 Make conflict dev
 Make Conflict developer_one
-
 Developer_one
 ------------------------
 24th Novemver 2023.
