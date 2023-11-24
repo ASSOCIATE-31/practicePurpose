67c30c06b6e575747211abbf4819aa79a872b846
Arka Bhattacharyya <arka@digitalaptech.co.in>
2023-11-24 14:16:07 +0530

    developer_one 4

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,14 +1,12 @@
 From developer_two  
 From developer_one  
 From dev branch
-
 Make conflict loper_two
 Make conflict developer_one
 Make conflict dev
 Make Conflict developer_one
-
 Developer_one
 ------------------------
-24th Novemver 2023.
+24th November 2023.
 
 
